cb5c7e4313b90e36dc3fe6c010558934f6d08246
James Young <james@welcomebrand.co.uk>
2014-01-02 17:29:34 +0000

    Updated links to most recent jQuery version

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php get_sidebar(); ?>    
+<?php get_sidebar(); ?>
 
 <footer role="contentinfo">
 	<p>&copy;<?php echo date("Y"); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
@@ -6,8 +6,8 @@
 
 <?php wp_footer(); ?>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-1.9.0.js"%3E%3C/script%3E'))</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-2.0.3.js"%3E%3C/script%3E'))</script>
 <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/general.js"></script>
 <?php if ( is_singular() ) wp_print_scripts( 'comment-reply' ); ?>
 </body>
